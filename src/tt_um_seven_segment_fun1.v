d7b848b3a85522bbb9b57e542474280de1e0e6bb
akaArmin <k12102711@students.jku.at>
2023-12-21 09:52:01 +0100

    wire

--- src/tt_um_seven_segment_fun1.v
+++ src/tt_um_seven_segment_fun1.v
@@ -5,14 +5,14 @@ module tt_um_seven_segment_fun1 #( parameter MAX_COUNT = 24'd10_000_000 ) (
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input1=output)
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
 
     // Pin assignment
-    clk = io_in[0]                  // Input Clock -> 10MHz
+    wire clk = io_in[0]                  // Input Clock -> 10MHz
     wire reset = ! rst_n;           // Reset
     wire btn1_incAni = io_in[1]      // Switch forward to the next Animation
     wire btn2_decAni = io_in[2]      // Switch backwards to the previous Animation

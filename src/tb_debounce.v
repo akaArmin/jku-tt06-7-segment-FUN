d8ecbe78c90ced11f1d1032e8b7740c8f5634a91
akaArmin <130210549+akaArmin@users.noreply.github.com>
2023-12-24 15:46:35 +0100

    Update tb_debounce.v

--- src/tb_debounce.v
+++ src/tb_debounce.v
@@ -2,189 +2,79 @@
 `timescale 1ns/1ps 
 
 /* 
-
 iverilog -g2005 -o FUN tb.v tt_um_seven_segment_fun_akaArmin.v decoder.v 
-
 vvp FUN 
-
 gtkwave tb.vcd 
-
 */ 
 
 // testbench is controlled by test.py 
-
 module tb_debounce; 
 
     // TB Signals 
-
-    reg clk = 0;        // Clock 
-    reg rst_n = 1;      // Reset (activ low) 
-    reg ena = 1;        // Enable 
+    reg clk;
+    reg rst_n;
     reg [7:0] ui_in; 
-    reg [7:0] uio_in; 
     wire [7:0] uo_out; 
-    wire [7:0] uio_out;
-    wire [7:0] uio_oe;
- 
 
     wire [6:0] segments = uo_out[6:0]; 
 
-    wire btn1 = ui_in[0]; 
-    wire btn2 = ui_in[1]; 
-    wire btn3 = ui_in[2]; 
-    wire btn4 = ui_in[3]; 
+    // Outputs
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
+    wire ena;
         
 
-    // Instantiate the Unit Under Test -> UUT 
-
+    // Instantiate the Unit Under Test -> UUT
     tt_um_seven_segment_fun1 uut ( 
         .ui_in      (ui_in),    // Dedicated inputs 
         .uo_out     (uo_out),   // Dedicated outputs 
         .uio_in     (uio_in),   // IOs: Input path 
         .uio_out    (uio_out),  // IOs: Output path 
         .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output) 
-
         .ena        (ena),      // enable - goes high when design is selected 
-
         .clk        (clk),      // clock 
-
         .rst_n      (rst_n)     // not reset 
-
     ); 
 
- 
-
     // Clock Generation -> 10MHz = 50ns 
+    // always #50 clk = ~clk; 
 
-    always #50 clk = ~clk; 
-
-     
 
     initial begin 
+        clk = 0;
+        always #50 clk = ~clk;
+    end
 
-        $dumpfile ("tb_debounce.vcd"); 
-
-        $dumpvars (0, tb_debounce); 
-
-         
-
+    initial begin
         // Reset 
-
         rst_n = 0; 
         #20; 
         rst_n = 1; 
         #40; 
 
-        btn1 = 1; 
-        #100; 
-        btn1 = 0;
-        #100; 
-        btn1 = 1;
-        #100; 
-        btn1 = 0; 
-        #100; 
-        btn1 = 1; 
-        #100; 
-        btn1 = 0; 
-        #100; 
-        btn1 = 1; 
-        #100; 
-        btn1 = 0; 
-        #100; 
-        btn1 = 0; 
+        // Button 1 press and release
+        ui_in = 8'b00000001;
+        #10 ui_in = 8'b00000000;
+        #100;
 
- 
+        // Button 2 press and release
+        ui_in = 8'b00000010;
+        #10 ui_in = 8'b00000000;
+        #100;
 
-        btn2 = 1; #100; 
+        // Button 3 press and release
+        ui_in = 8'b00000100;
+        #10 ui_in = 8'b00000000;
+        #100;
 
-        btn2 = 0; #100; 
+        // Button 4 press and release
+        ui_in = 8'b00001000;
+        #10 ui_in = 8'b00000000;
+        #100;
 
-        btn2 = 1; #100; 
-
-        btn2 = 0; #100; 
-
-        btn2 = 1; #100; 
-
-        btn2 = 0; #100; 
-
-        btn2 = 1; #100; 
-
-        btn2 = 0; #100; 
-
-        btn2 = 0; 
-
- 
-
-        btn3 = 1; #100; 
-
-        btn3 = 0; #100; 
-
-        btn3 = 1; #100; 
-
-        btn3 = 0; #100; 
-
-        btn3 = 1; #100; 
-
-        btn3 = 0; #100; 
-
-        btn3 = 1; #100; 
-
-        btn3 = 0; #100; 
-
-        btn3 = 0; 
-
- 
-
-        btn4 = 1; #100; 
-
-        btn4 = 0; #100; 
-
-        btn4 = 1; #100; 
-
-        btn4 = 0; #100; 
-
-        btn4 = 1; #100; 
-
-        btn4 = 0; #100; 
-
-        btn4 = 1; #100; 
-
-        btn4 = 0; #100; 
-
-        btn4 = 0; 
-
- 
-
-        $finish; 
-
-    end 
-
- 
-
-    /* 
-
-    task perform_test; 
-
-        for (integer i = 0; i < 250; i++) begin 
-
-            data_in = i;    // Generate 10-bit test data 
-
-            strobe_in = 1;  // Strobe signal active 
-
-            #20; 
-
-            strobe_in = 0;  // Strobe signal inactive 
-
-            #20; 
-
-        end 
-
-    endtask 
-
-    */ 
-
-endmodule 
-
- 
-
+        // Finish the simulation
+        #100 $finish;
+  end
+endmodule
  

1d1b7fad11bdd28c7e8feb744ee488a05e41bd77
akaArmin <130210549+akaArmin@users.noreply.github.com>
2023-12-30 18:44:41 +0100

    idk

--- src/tt_um_seven_segment_fun1.v
+++ src/tt_um_seven_segment_fun1.v
@@ -4,7 +4,7 @@
 `include "seg7.v"
 `include "changing.v"
 
-module tt_um_seven_segment_fun1 #( parameter MAX_COUNT = 24'd10_000_000 ) (
+module tt_um_seven_segment_fun1 (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
